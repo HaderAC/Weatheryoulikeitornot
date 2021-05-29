a1ff0200d50f091379cc1eba833ad28d510b0a15
HaderCore <Hadercortes@gmail.com>
2021-05-29 16:16:18 -0400

    Added js file and api to search for weather

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var wapi = "c7a6e4833c8f6120d2e8b2262378040e";
+var currentLoc = "";
+var prevLoc = "";
+
+// function for current weather on the map
+
+const currentW = (event)=>{
+    let loc = $("#citySearch").val();
+    currentLoc = $("#citySearch").val();
+
+    let queryURL = 
+}
+
