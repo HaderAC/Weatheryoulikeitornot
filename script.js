a8a5a04166293e09853e8175cab4f62a1769ec44
HaderCore <Hadercortes@gmail.com>
2021-06-01 22:06:52 -0400

    104 js commit for functionality

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ function init(){
     pastSearchButtonEl.prepend(pastSearchEl);
 }
 }
+
+
+var keyCount = 0;
+
+
+searchButton.click(function () {
+
+    var searchInput = $(".searchInput").val();
+
+    pastSearchEl = document.createElement("button");
+
+    pastSearchEl.textContent = searchInput;
+    pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+
+
+    pastSearchEl.setAttribute("data-city",searchInput)
+    pastSearchEl.setAttribute("type", "submit");
+
+
+    pastSearchButtonEl.prepend(pastSearchEl);
+    
+
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
